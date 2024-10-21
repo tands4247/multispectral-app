e067117f14f9d90e2ae81ec9012a47c6cf0c7a9f
tands4247 <tands4247@gmail.com>
2024-10-21 20:49:48 +0900

    MVCモデルにリファクタリング

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from screens import home_screen
-
-def main():
-    window = home_screen.Application()
-    window.mainloop()
+from view.home_screen import ApplicationView
+from controller.app_controller import ApplicationController
+import customtkinter
 
 if __name__ == '__main__':
-    main()
+    # root = customtkinter.CTk()
+    
+    controller = ApplicationController()
+    # view = ApplicationView(controller)
+    # view.grid(row=0, column=0, sticky="nsew")
+    # view.set_frames()
+
+    # アプリケーションの実行
+    controller.run()
     
--- view/home_screen.py
+++ view/home_screen.py
@@ -1,29 +1,23 @@
 import tkinter as tk
-from tkinter import filedialog
 import customtkinter
-import os
-import numpy as np
 from PIL import Image, ImageTk
-import matplotlib.pyplot as plt
-import tifffile as tiff
-# 作成モジュール
-from functions.preprocessing_images import PreprocessingImages
-from functions.calc_vegindex import CalcVegIndex
+# from controller.app_controller import ApplicationController
 
 FONT_TYPE = "meiryo"
 WINDOW_SIZE = "1350x750"
-INIT_DIR = 'C:/project/multispectral-app'
 
-class Application(customtkinter.CTk):
-    def __init__(self):
+class ApplicationView(customtkinter.CTk):
+    def __init__(self, controller):
         super().__init__()
         
+        self.controller = controller
         self.title('マルチスペクトル画像処理')
         self.geometry(WINDOW_SIZE)
         self.fonts = (FONT_TYPE, 15)
-        customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-        customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("dark-blue")
         
+    def set_frames(self):
         # MenuFrameの設定
         self.menu_frame = MenuFrame(self, width=70, height=700)
         self.menu_frame.grid(row=0, column=0, padx=15, pady=0, sticky="n")
@@ -37,17 +31,18 @@ class Application(customtkinter.CTk):
         self.veg_index_frame = VegIndexFrame(self, width=530, height=750)
         self.veg_index_frame.grid(row=0, column=2, padx=15, pady=0, sticky="n")
         self.veg_index_frame.grid_propagate(False)   # フレームを固定
-
+    
 
 class MenuFrame(customtkinter.CTkFrame):
-    def __init__(self, window=None, width=None, height=None):
+    def __init__(self, window, width=None, height=None):
         super().__init__(window, width=width, height=height)
+        self.controller = window.controller
         self.create_widgets()
     
     def create_widgets(self):
         # フォルダ選択ボタンの配置
         self.button_select_dir = customtkinter.CTkButton(self, text='フォルダ選択',
-                                                            command=self.select_dir_callback)
+                                                            command=self.controller.select_dir_callback)
         self.button_select_dir.grid(row=0, padx=10, pady=(15,0), sticky="w")
         
         # フォルダ名表示ラベル
@@ -56,38 +51,27 @@ class MenuFrame(customtkinter.CTkFrame):
         
         # 前処理開始ボタン
         self.button_start_proccesing = customtkinter.CTkButton(self, text='前処理開始', fg_color="#696969",
-                                                            command=self.start_processing_callback)
+                                                            command=self.controller.start_processing_callback)
         self.button_start_proccesing.grid(row=2, padx=10, pady=(15,0), sticky="w")
         
         # 表示バンド - ラジオボタン
         self.label_select_band = customtkinter.CTkLabel(self, text='表示するバンドを選択')
         self.label_select_band.grid(row=3, padx=10, pady=(50,0), sticky="w")
-        
+
         self.radio_var_band = tk.IntVar(value=0)
-        self.radiobutton_select_band1 = customtkinter.CTkRadioButton(self,
-                                        text='DataCube', command=self.radbutton_event_select_band,
-                                        variable= self.radio_var_band, value=5)
-        self.radiobutton_select_band1.grid(row=4, padx=10, pady=(10,0), sticky="w")
-        
-        self.radiobutton_select_band2 = customtkinter.CTkRadioButton(self,
-                                        text='Green', command=self.radbutton_event_select_band,
-                                        variable= self.radio_var_band, value=1)
-        self.radiobutton_select_band2.grid(row=5, padx=10, pady=(10,0), sticky="w")
-        
-        self.radiobutton_select_band3 = customtkinter.CTkRadioButton(self,
-                                        text='Red', command=self.radbutton_event_select_band,
-                                        variable= self.radio_var_band, value=2)
-        self.radiobutton_select_band3.grid(row=6, padx=10, pady=(10,0), sticky="w")
-        
-        self.radiobutton_select_band4 = customtkinter.CTkRadioButton(self,
-                                        text='Red-Edge', command=self.radbutton_event_select_band,
-                                        variable= self.radio_var_band, value=3)
-        self.radiobutton_select_band4.grid(row=7, padx=10, pady=(10,0), sticky="w")
-        
-        self.radiobutton_select_band5 = customtkinter.CTkRadioButton(self,
-                                        text='NIR', command=self.radbutton_event_select_band,
-                                        variable= self.radio_var_band, value=4)
-        self.radiobutton_select_band5.grid(row=8, padx=10, pady=(10,0), sticky="w")
+
+        bands = [("DataCube", 5), ("Green", 1), ("Red", 2), ("Red-Edge", 3), ("NIR", 4)]
+
+        for idx, (text, value) in enumerate(bands, start=4):
+            radiobutton = customtkinter.CTkRadioButton(
+                self,
+                text=text,
+                command=self.controller.radbutton_event_select_band,
+                variable=self.radio_var_band,
+                value=value
+            )
+            radiobutton.grid(row=idx, padx=10, pady=(10, 0), sticky="w")
+
         
         # 表示 植生指数 - ラジオボタン
         self.label_select_vegindex = customtkinter.CTkLabel(self, text='表示する植生指数を選択')
@@ -95,44 +79,11 @@ class MenuFrame(customtkinter.CTkFrame):
         
         self.radio_var_vegindex = tk.IntVar(value=0)
         self.radiobutton_select_ndvi = customtkinter.CTkRadioButton(self,
-                                        text='NDVI', command=self.radbutton_event_select_vegindex,
+                                        text='NDVI', command=self.controller.radbutton_event_select_vegindex,
                                         variable= self.radio_var_vegindex, value=1)
         self.radiobutton_select_ndvi.grid(row=10, padx=10, pady=(10,0), sticky="w")
-    
-    
-    # フォルダ選択のコールバック
-    def select_dir_callback(self):
-        init_dir = INIT_DIR if os.path.exists(INIT_DIR) else os.path.expanduser('~')
-        # フォルダ選択のダイアログを開く
-        self.select_dir_path = tk.filedialog.askdirectory(initialdir=init_dir)
         
-        if self.select_dir_path:
-            folder_name = os.path.basename(self.select_dir_path)
-            self.label_dir_name.configure(text=f"フォルダ名: {folder_name}")
-    
-    # 前処理開始のコールバック
-    def start_processing_callback(self):        
-        preprocessor = PreprocessingImages(self.select_dir_path)
-        image_8bit_list = preprocessor.bit_convert()
-        self.datacube_list = preprocessor.make_datacube()
-        
-        vegindex_processor = CalcVegIndex(self.datacube_list)
-        self.ndvi_list = vegindex_processor.calc_NDVI()
-
-        
-    # ラジオボタンのイベント（表示するバンドを選択）
-    def radbutton_event_select_band(self):
-        self.display_band_index = self.radio_var_band.get()
-        self.master.spectral_img_frame.display_spectral(self.datacube_list, self.display_band_index)
-        
-    
-    def radbutton_event_select_vegindex(self):
-        # if self.radio_var_vegindex == 1:
-        #     self.
-        self.master.veg_index_frame.display_vegIndex(self.ndvi_list)
-        
-        
-        
+import numpy as np
 class SpectralImgFrame(customtkinter.CTkFrame):
     def __init__(self, window=None, width=None, height=None):
         super().__init__(window, width=width, height=height)
